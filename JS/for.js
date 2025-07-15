241ff555653ad1e9fa6cf34e95f187e2f5b93371
Abhay Singh <abhaysingh03032002@gmail.com>
2025-07-15 08:13:20 +0530

    for

--- JS/for.js
+++ JS/for.js
@@ -12,12 +12,16 @@
 // const IronMan = {
 //   name: "Tony stark",
 //   age: 45,
-//   friends: ["Steve", "peter", "Rhody", "Banner"],
+//   friends: ["Steve", "peter", "Rhody", "Banner"],           Arrays do have an .entries() method built-in.
 // };
 
-// for (const friend of IronMan.friends) {
-//   if (friend === "peter") continue; // skip peter
-//   console.log(friend);
+// // for (const friend of IronMan.friends) {
+// //   if (friend === "peter") continue; // skip peter
+// //   console.log(friend);
+// // }
+
+// for (const [index, name] of IronMan.friends.entries()) {
+//   console.log("Number of tony friends", `${index} and ${name}`);
 // }
 
 //----------------------------------------------
@@ -28,10 +32,20 @@ Arrays
 Objects (via Object.entries())
 */
 
-let fruits = ["apple", "banana", "cherry"];
-let iterator = fruits.entries();
-console.log(iterator); //Array iterator {}
+// let fruits = ["apple", "banana", "cherry"];
+// let iterator = fruits.entries();
+// console.log(iterator); //Array iterator {}
 
-for (let [index, value] of iterator) {
-  console.log(index, value);
+// for (let [index, value] of iterator) {
+//   console.log(index, value);
+// }
+
+let person = {
+  name: "Abhay",
+  age: 23,
+  address: { city: "Mohania", PinCode: 821109 },
+}; //Plain JavaScript objects do NOT have a .entries() method.
+
+for (const [key, value] of Object.entries(person)) {
+  console.log(`At key '${key}' value is ${value}`);
 }
--- JS/tempCodeRunnerFile.js
+++ JS/tempCodeRunnerFile.js
@@ -1,3 +1,14 @@
-for (let [index, value] of iterator) {
-//   console.log(index, value);
+const IronMan = {
+//   name: "Tony stark",
+//   age: 45,
+//   friends: ["Steve", "peter", "Rhody", "Banner"],
 // };
+
+// // for (const friend of IronMan.friends) {
+// //   if (friend === "peter") continue; // skip peter
+// //   console.log(friend);
+// // }
+
+// for (const [index, name] of IronMan.friends.entries()) {
+//   console.log("Number of tony friends", `${index} and ${name}`);
+// }
